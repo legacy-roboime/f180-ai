8da6a9934d27f6562ecb6b3763ebc3f6b9f17974
Luciano Barreira <meckmao@gmail.com>
2016-08-19 11:11:20 -0300

    Inputs now const and point dist defined

--- amb/utils.cpp
+++ amb/utils.cpp
@@ -1,15 +1,20 @@
 #include "utils.h"
 
-float util::rad(float deg){
+float util::rad(const float deg) {
     return (PI*deg/180.0);
 }
 
-float util::deg(float rad){
+float util::deg(const float rad) {
     return (180.0*rad/PI);
 }
 
-// Set an angle to (-PI,PI) value range
-float util::wrap(float ang){
+float util::wrap(const float ang) {
     return (fmod(ang + 5 * PI, 2 * PI) - PI);
+}
+
+float util::dist(const Pose p1, const Pose p2) {
+    const float dx_2 = (p1.x_-p2.x_)*(p1.x_-p2.x_);
+    const float dy_2 = (p1.y_-p2.y_)*(p1.y_-p2.y_);
+    return sqrt(dx_2+dy_2);
 }
 
--- amb/utils.h
+++ amb/utils.h
@@ -2,16 +2,18 @@
 
 #include <cmath>
 
+#include "pose.h"
+
 #define PI 3.14159265358979323846
 
-// TODO: move these constants to control dir
+// TODO: move these constants to control dir when rdy
 const float PW = 1.80f;
 const float PL = 1.50f;
 
-// utility functions for the sake of simplicity
 namespace util{
-    float rad(float deg); 
-    float deg(float rad);
-    float wrap(float ang);
+    float rad(const float deg) ; 
+    float deg(const float rad) ; 
+    float wrap(const float ang) ;
+    float dist(const Pose p_1, const Pose p_2);
 }
 
